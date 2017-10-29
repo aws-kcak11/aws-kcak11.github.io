8064a80d85455631007dc771d977dc203f5a4589
K.C.Ashish Kumar <kcak11@gmail.com>
2017-10-29 09:38:49 +0530

    Create fluid-iframe-container.js

---
+++ scripts/fluid-iframe-container.js
@@ -0,0 +1,41 @@
+(function() {
+	window.fluidIframeContainer = function(url) {
+		var init = function() {
+			var divContainer = document.createElement("div");
+			divContainer.id = "mainContainer";
+			divContainer.style.position = "fixed";
+			divContainer.style.top = divContainer.style.right = divContainer.style.bottom = divContainer.style.left = "0px";
+			document.getElementsByTagName("body")[0].appendChild(divContainer);
+
+			var frameContainer = document.createElement("iframe");
+			frameContainer.id = "mainFrame";
+			frameContainer.src = url;
+			frameContainer.style.display = "block";
+			frameContainer.style.border = "none";
+			frameContainer.style.position = "absolute";
+			frameContainer.style.top = frameContainer.style.right = frameContainer.style.bottom = frameContainer.style.left = "0px";
+			frameContainer.style.width = divContainer.offsetWidth + "px";
+			frameContainer.style.height = divContainer.offsetHeight + "px";
+			document.getElementById("mainContainer").appendChild(frameContainer);
+
+			return {
+				"mainContainer" : document.getElementById("mainContainer"),
+				"mainFrame" : document.getElementById("mainFrame")
+			};
+		};
+		var elements = init();
+
+		var autoAdjustDimensions = function() {
+			function fixDim() {
+				elements.mainFrame.style.width = elements.mainContainer.offsetWidth + "px";
+				elements.mainFrame.style.height = elements.mainContainer.offsetHeight + "px";
+			}
+			if (window.attachEvent) {
+				window.attachEvent("onresize", fixDim);
+			} else if (window.addEventListener) {
+				window.addEventListener("resize", fixDim, false);
+			}
+		};
+		autoAdjustDimensions();
+	};
+}());
